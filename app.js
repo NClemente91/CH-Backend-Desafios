1e8c41422331ed611d3362f6eed8923298e0aac3
Nicolás Clemente <nicoclemente91@gmail.com>
2021-08-20 08:25:40 -0300

    Desafio 11B - EJS

---
+++ app.js
@@ -0,0 +1,47 @@
+const express = require("express");
+path = require("path");
+
+class Server {
+  constructor() {
+    this.app = express();
+    this.port = 8080;
+
+    this.middlewares();
+
+    this.ejs();
+
+    this.routes();
+  }
+
+  middlewares = () => {
+    this.app.use(express.static(__dirname + "/public"));
+  };
+
+  routes = () => {
+    this.app.use(express.json());
+    this.app.use(express.urlencoded({ extended: true }));
+    this.app.use("/api", require("./routes/productos.routes"));
+  };
+
+  ejs = () => {
+    //Establecemos el motor de plantilla que se utiliza
+    this.app.set("view engine", "ejs");
+
+    //Establecemos el directorio donde se encuentran los archivos de plantilla
+    this.app.set("views", "./views");
+  };
+
+  start = () => {
+    this.app.listen(this.port, () => {
+      try {
+        console.log(`Servidor conectado en puerto ${this.port}`);
+      } catch (error) {
+        console.log("Error de conexión", error);
+      }
+    });
+  };
+}
+
+const server = new Server();
+
+server.start();
