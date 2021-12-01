09c3efd6e25465e50e70453d7f8095bd0c1ca60d
Nicolás Clemente <nicoclemente91@gmail.com>
2021-12-01 11:16:03 -0300

    Add nodemailer

--- configs/passport.js
+++ configs/passport.js
@@ -1,6 +1,7 @@
 const passport = require("passport");
 const bCrypt = require("bcrypt");
 const LocalStrategy = require("passport-local").Strategy;
+const gmail = require("../configs/email/gmail");
 
 const User = require("../components/users/users.model");
 const { loggerInfo, loggerWarn, loggerError } = require("./loggers");
@@ -73,6 +74,21 @@ passport.use(
                 loggerError.error(`Error in Saving user ${err}`);
                 throw err;
               }
+              const asunto = "Nuevo Registro";
+              const to = process.env.EMAIL_ADMIN;
+              let mensaje = `Se registró un nuevo usuario cuyo email es ${
+                newUser.email
+              } en la fecha ${new Date().toLocaleString()}`;
+              let adjunto = "";
+              gmail.enviarMail(asunto, mensaje, adjunto, to, (err, info) => {
+                if (err) {
+                  loggerInfo.info(`Error en nodemailer-gmail ${err}`);
+                  loggerError.error(`Error en nodemailer-gmail ${err}`);
+                } else {
+                  loggerInfo.info(`Email enviado con nodemailer-gmail ${info}`);
+                }
+              });
+
               loggerInfo.info(`User Registration succesful`);
               return done(null, newUser);
             });
@@ -89,5 +105,5 @@ const isValidPassword = (user, password) => {
 };
 
 const createHash = (password) => {
-  return bCrypt.hashSync(password.toString(), bCrypt.genSaltSync(10), null);
+  return bCrypt.hashSync(password, bCrypt.genSaltSync(10), null);
 };
